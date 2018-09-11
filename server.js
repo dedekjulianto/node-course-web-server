27c9d2349b8adccb29145d6ea65fe33f01fc66af
dedekjulianto <dedekproject@gmail.com>
2018-09-12 06:06:30 +0700

    Add project page

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+app.get('/project', (req, res) => {
+	res.render("project.hbs", {
+		pageTitle: "Projects"
+	});
+})
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
